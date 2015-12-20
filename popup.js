3b322c41a903c56796b023fe72399b0b53d76020
johorst <mart1601@web.de>
2015-12-20 22:03:43 +0100

    empfaenger wird aus html page gelesen sender ist hardcoded in popup.js es wird ein http request mit 2 parametern generiert viel ballast, error callback nicht mitgegeben... geht request eigentlich bei click oder load los?? hm

--- popup.js
+++ popup.js
@@ -1,13 +1,4 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
 function getCurrentTabUrl(callback) {
   // Query filter to be passed to chrome.tabs.query - see
   // https://developer.chrome.com/extensions/tabs#method-query
@@ -17,37 +8,18 @@ function getCurrentTabUrl(callback) {
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
     var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
     var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
     console.assert(typeof url == 'string', 'tab.url should be a string');
     callback(url);
-	chrome.tabs.executeScript(null, { file: "jquery-1.11.3.min.js" }, function() {
-		chrome.tabs.executeScript(null, { code: 'alert("Konto Nr. " + $("#mgm_id").attr("mgm_kto"))' });
-	});
+    var myid ="1234-1234-12345678";
+    var empfaengerid = "";
+    chrome.tabs.executeScript(null, { file: "jquery-1.11.3.min.js" }, function() {
+	chrome.tabs.executeScript(null, { code: 'var d = $("#mgm_id").attr("mgm_kto"); d '},
+		function(results){alert("Empfaenger Kto. Nr. " + results + " \nGeber: " + myid);callBank(null, null, null, results, myid)});
 	//<div id="mgm_id" mgm_kto="6">
+    });
   });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
 /**
@@ -58,8 +30,6 @@ function getCurrentTabUrl(callback) {
  *   The callback gets a string that describes the failure reason.
  */
 function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
   var searchUrl = ' https://clickcounterbutton-johorst.c9.io/registerID?id=fff';
   var x = new XMLHttpRequest();
   x.open('GET', searchUrl);
@@ -89,6 +59,38 @@ function getImageUrl(searchTerm, callback, errorCallback) {
   };
   x.send();
 }
+
+function callBank(searchTerm, callback, errorCallback, sender, empfaenger) {
+  var searchUrl = ' https://clickcounterbutton-johorst.c9.io/registerID?sender=' + sender + '&empfaenger=' + empfaenger;
+  var x = new XMLHttpRequest();
+  x.open('GET', searchUrl);
+  // The Google image search API responds with JSON, so let Chrome parse it.
+  x.responseType = 'json';
+  x.onload = function() {
+    // Parse and process the response from Google Image Search.
+    var response = x.response;
+    if (!response || !response.responseData || !response.responseData.results ||
+        response.responseData.results.length === 0) {
+      errorCallback('No response from Google Image search!');
+      return;
+    }
+    var firstResult = response.responseData.results[0];
+    // Take the thumbnail instead of the full image to get an approximately
+    // consistent image size.
+    var imageUrl = firstResult.tbUrl;
+    var width = parseInt(firstResult.tbWidth);
+    var height = parseInt(firstResult.tbHeight);
+    console.assert(
+        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
+        'Unexpected respose from the Google Image Search API!');
+    callback(imageUrl, width, height);
+  };
+  x.onerror = function() {
+    errorCallback('Network error.');
+  };
+  x.send();
+}
+
 
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
