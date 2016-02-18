92b1de7d176119829448aa4fb11e8557d3257db8
Grillo <mnkuemme@pharmatechnik.de>
2016-02-18 15:51:37 +0100

    Kommentare v Google entfernt

--- popup.js
+++ popup.js
@@ -1,6 +1,4 @@
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
   var queryInfo = {
     active: true,
     currentWindow: true
@@ -25,10 +23,8 @@ function callBank(searchTerm, callback, errorCallback, sender, empfaenger) {
   var searchUrl = 'http://40.112.143.137:8080/?name=' + sender + '&empfaenger=' + empfaenger;
   var x = new XMLHttpRequest();
   x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
   x.responseType = 'json';
   x.onload = function() {
-    // Parse and process the response from Google Image Search.
     var response = x.response;
     if (!response || !response.responseData || !response.responseData.results ||
         response.responseData.results.length === 0) {
@@ -36,8 +32,6 @@ function callBank(searchTerm, callback, errorCallback, sender, empfaenger) {
       return;
     }
     var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
     var imageUrl = firstResult.tbUrl;
     var width = parseInt(firstResult.tbWidth);
     var height = parseInt(firstResult.tbHeight);
